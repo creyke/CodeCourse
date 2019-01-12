71e295b76d9cc7ed26bf326d1cbea1380bbd7f88
Roger Creyke <creyke@gmail.com>
2019-01-12 20:03:10 +0000

    Week 001 completed code.

---
+++ Source/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			for (var loopNumber = 0; loopNumber < 10; loopNumber++)
+			{
+				Console.WriteLine("Hello there, I am at loop number " + loopNumber);
+			}
+        }
+    }
+}
